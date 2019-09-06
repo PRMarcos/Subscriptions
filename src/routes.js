1bae09c7c8baced371dc237d81f64f66dee0aa10
Marcos Ramos <marcos.zion.crew@gmail.com>
2019-09-05 23:34:34 -0300

    routes changed to reach EncontreirosForm page

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import { List } from "./pages/List";
 import LoginPage from "./pages/Login";
 import { AuthProvider } from "./services/auth";
 import { PrivateRoute } from "./PrivateRoute";
+import { EncontreirosFrom } from "./pages/EncontreirosFrom";
 
 export default function Routes() {
   return (
@@ -15,6 +16,12 @@ export default function Routes() {
           <Route path="/" exact component={Main} />
           <PrivateRoute path="/list" exact component={List} />
           <Route path="/autentication" exact component={LoginPage} />
+          <Route path="/encontreiro" exact component={EncontreirosFrom} />
+          <Route
+            path="/encontrista"
+            exact
+            component={() => <h1>Em construçao! ☺ </h1>}
+          />
           <Route path="*" component={() => <h1>Page not found</h1>} />
         </Switch>
       </BrowserRouter>
