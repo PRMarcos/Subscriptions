802e1a31b38423d4be91e84f3d923c6a550cc419
Marcos Ramos <marcos.zion.crew@gmail.com>
2019-09-11 18:07:44 -0300

    The export to xlsx was implemented

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -2,10 +2,35 @@ import React from "react";
 import { NavBar } from "../../components/NavBar";
 import { Button } from "../../components/Button";
 import { LogOut } from "../../services/firestore";
-
+import { Listar } from "../../services/firestore";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { Container, OptionContainer } from "./style";
+import * as FileSaver from "file-saver";
+import * as XLSX from "xlsx";
 
 const Admin = ({ history }) => {
+  async function handleExport() {
+    try {
+      const Encontreiros = await Listar("Encontreiros");
+      const Encontristas = await Listar("Inscricoes");
+
+      const fileType =
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+      const fileExtension = ".xlsx";
+
+      const encontristas = XLSX.utils.json_to_sheet(Encontristas);
+      const encontreiros = XLSX.utils.json_to_sheet(Encontreiros);
+
+      const wb = { Sheets: { 'encontristas': encontristas, 'encontreiros': encontreiros }, SheetNames: ['encontristas', 'encontreiros'] };
+      const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+      const data = new Blob([excelBuffer], { type: fileType });
+      FileSaver.saveAs(data, "RelatorioExport" + fileExtension);
+
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
   return (
     <Container>
       <NavBar
@@ -42,15 +67,11 @@ const Admin = ({ history }) => {
         >
           Listar Encontreiros
         </Button>
-        <Button
-          small
-          btnFunc={() => {
-            alert("funçao ainda nao implementada.");
-          }}
-        >
+        <Button small btnFunc={handleExport}>
           Exportar
         </Button>
       </OptionContainer>
+      <ToastContainer />
     </Container>
   );
 };
